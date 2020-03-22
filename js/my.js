d6493528c56b54961aefe29c767b732a6a36f125
Hanrun Li <hanrunli@freewheel.com>
2020-03-21 22:19:14 -0400

    haha

--- js/my.js
+++ js/my.js
@@ -2,22 +2,45 @@
 
 var pressed = {},
     audio = document.getElementById('sound'),
-    output = document.getElementById('output'),
-    maxValue = 2,
-    duration,
-    volume;
+    trump = document.getElementById('trump'),
+    cha_btn = document.getElementById('cha_btn'),
+    xiaohao_btn = document.getElementById('xiaohao_btn'),
+    btn_group = document.getElementById('btn_group'),
+    isKeyDown = false;
+
+btn_group.addEventListener('click', (event) => {
+    if (isKeyDown)
+        return;
+
+    audio.load();
+    isKeyDown = true;
+
+    if (event.target.id == "cha_btn") {
+        audio.play();
+        trump.src = "img/cha.jpg";
+    }
+
+    if (event.target.id == "xiaohao_btn") {
+        audio.play();
+        trump.src = "img/xiaohao.jpg";
+    }
+
+    isKeyDown = false;
+})
 
 document.onkeydown = function (e) {
-    audio.load();
-
     // 79 = Q
     if (e.keyCode === 81) {
-        audio.volume = 1;
-        audio.play();
-        return false;
+        cha_btn.click();
+    }
+
+    // 87 = W
+    if (e.keyCode === 87) {
+        xiaohao_btn.click();
     }
 };
 
 document.onkeyup = function (e) {
-
+    trump.src = "img/imrs.jpg";
+    isKeyDown = false;
 };
